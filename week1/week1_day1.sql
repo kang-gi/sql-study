1edc2f36efc81b6af74f0e7578f8d21bceefc0c1
kang-gi <kkm101555@gmail.com>
2026-05-10 21:33:09 +0900

    Create week1_day1.sql

---
+++ week1/week1_day1.sql
@@ -0,0 +1,5 @@
+SELECT col_1, col_2, col_4, col_5, col_6 FROM SDoT WHERE col_5 = '성동구';
+
+SELECT col_4, col_5, col_6, col_2 FROM SDoT WHERE col_2 > 4500 ORDER BY col_2 DESC;
+
+SELECT DISTINCT g FROM SDoT;
